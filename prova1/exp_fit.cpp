8a8496e45bf6d1ee526bf849db25034b67310aa4
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2022-11-28 19:40:00 +0100

    Germanium

--- prova1/exp_fit.cpp
+++ prova1/exp_fit.cpp
@@ -1,9 +1,13 @@
 #include <cmath>
+#include <fstream>
 #include <iostream>
 
 #include "TCanvas.h"
 #include "TF1.h"
+#include "TFitResult.h"
 #include "TGraphErrors.h"
+#include "TLatex.h"
+#include "TMatrixD.h"
 
 Double_t exponential(Double_t* x, Double_t* par) {
   Double_t xx = x[0];
@@ -12,17 +16,30 @@ Double_t exponential(Double_t* x, Double_t* par) {
 }
 
 void exp_fit() {
-  // gStyle->SetOptFit(1111);
+  gStyle->SetOptFit(11);
+  gStyle->SetFitFormat("5.2g");
 
-  Double_t yy[16] = {0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.09, 0.12,
-                     0.13, 0.17, 0.23, 0.32, 0.48, 0.70, 1.16, 1.75};
-  Double_t xx[16] = {360, 380, 400, 420, 440, 450, 460, 470,
-                     480, 490, 500, 520, 540, 560, 580, 600};
-  Double_t ey[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-  Double_t ex[16] = {15, 15, 16, 16, 24, 17, 17, 17,
-                     25, 18, 18, 25, 43, 26, 44, 27};
+  Double_t ey[18] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  Double_t xx[18];
+  Double_t yy[18];
+  Double_t ex[18];
+  std::ifstream myfile;
+  myfile.open("exp_Si.txt");
+  double a;
+  int j = 0;
+  while (myfile >> a) {
+    if ((j % 3) == 0) {
+      xx[j / 3] = a;
+    } else if ((j % 3) == 1) {
+      yy[j / 3] = a;
+    } else {
+      ex[j / 3] = a;
+    }
+    ++j;
+  }
+  myfile.close();
 
-  TGraphErrors* gr1 = new TGraphErrors(16, xx, yy, ex, ey);
+  TGraphErrors* gr1 = new TGraphErrors(18, xx, yy, ex, ey);
   gr1->SetTitle("Exponential Fit");
   gr1->SetMarkerStyle(kFullCircle);
   gr1->SetMarkerColor(kBlue);
@@ -30,14 +47,15 @@ void exp_fit() {
   // gr1->SetFillColor(kGreen);
   // gr1->SetFillStyle(3010);
   gr1->GetXaxis()->SetTitle("V (mV)");
-  gr1->GetYaxis()->SetTitle("A (mA)");
+  gr1->GetYaxis()->SetTitle("I (mA)");
 
-  TF1* f1 = new TF1("f1", exponential, 340, 700, 2);
+  TF1* f1 = new TF1("f1", exponential, 340, 600, 2);
   f1->SetParameters(6e-6, 47);
-  gr1->Fit("f1", "RY0, Q");
+  gr1->Fit("f1", "EY0, R, Q");
   TF1* fit1 = gr1->GetFunction("f1");
   fit1->SetLineColor(kGreen);
   fit1->SetLineWidth(2);
+    fit1->SetParNames("I_{0}", "#etaV_{T}");
 
   double chi_square = fit1->GetChisquare() / fit1->GetNDF();
   double I_0 = fit1->GetParameter(0);
@@ -50,6 +68,7 @@ void exp_fit() {
             << "\nReduced Chi Square: " << chi_square << '\n';
 
   TCanvas* can = new TCanvas("can", "canvas", 200, 10, 600, 400);
+  can->SetGrid();
   gr1->Draw("AP");
   fit1->Draw("L, SAME");
 }
